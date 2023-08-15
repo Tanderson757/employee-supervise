cdf8603e452238be86b19ba98cd4c9bd4df701ad
Tanderson757 <125073429+Tanderson757@users.noreply.github.com>
2023-08-15 09:38:59 -0400

    added comments to db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,8 @@
+-- Drop the 'employee_tracker_db' database if it already exists
 DROP DATABASE IF EXISTS employee_tracker_db;
+
+-- Create a new database named 'employee_tracker_db'
 CREATE DATABASE employee_tracker_db;
+
+-- Switch to using the newly created 'employee_tracker_db' database
 USE employee_tracker_db;
