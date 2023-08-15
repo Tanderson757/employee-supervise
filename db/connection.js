9d610df632833ecb7b1aa15bad844421b25280a6
Tanderson757 <125073429+Tanderson757@users.noreply.github.com>
2023-08-15 11:47:43 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Create a connection to the MySQL database
 const db = mysql.createConnection({
-    host: 'localhost',          // Replace with your MySQL server hostname
-    user: 'root',               // Your MySQL username
-    password: 'new_password',   // Your MySQL password
-    database: 'employee_tracker_db' // Name of the database you want to connect to
+    host: 'localhost',          //  MySQL server hostname
+    user: 'root',               //  MySQL username
+    password: 'new_password',   //  MySQL password
+    database: 'employee_tracker_db' // Name of the database 
 });
 
 // Export the 'db' connection object to be used in other parts of the application
