df718e59932976898dc18bbab9eb21b1ccb1a6a4
Tanderson757 <125073429+Tanderson757@users.noreply.github.com>
2023-08-15 11:48:22 -0400

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,7 @@
--- Drop the 'employee_tracker_db' database if it already exists
+-- Drop the 'employee_tracker_db' database 
 DROP DATABASE IF EXISTS employee_tracker_db;
 
--- Create a new database named 'employee_tracker_db'
+-- Created a new database named 'employee_tracker_db'
 CREATE DATABASE employee_tracker_db;
 
 -- Switch to using the newly created 'employee_tracker_db' database
