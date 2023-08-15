b08d9669e32aac02edf0a9575fe923ffba224798
Tanderson757 <125073429+Tanderson757@users.noreply.github.com>
2023-08-15 09:37:38 -0400

    added comments to connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,13 @@
+// Import the 'mysql2' module to establish a connection with MySQL database
 const mysql = require('mysql2');
-//require('dotenv').config();
 
+// Create a connection to the MySQL database
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password
-    password: 'new_password',
-    database: 'employee_tracker_db'
+    host: 'localhost',          // Replace with your MySQL server hostname
+    user: 'root',               // Your MySQL username
+    password: 'new_password',   // Your MySQL password
+    database: 'employee_tracker_db' // Name of the database you want to connect to
 });
 
+// Export the 'db' connection object to be used in other parts of the application
 module.exports = db;
